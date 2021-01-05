b0269f720298d312a745b45bf60cd65f4f3c3838
AshwiniNam <knambudiri4@gmail.com>
2021-01-05 10:38:54 +0530

    taking array size from the user

--- Arrays/ReverseArray.cpp
+++ Arrays/ReverseArray.cpp
@@ -1,1 +1,32 @@
 //Reverse the array
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int inputarray[30],i,temp=0,size;
+	cout<<"Enter the size of the array: ";
+	cin>>size;
+	
+	//taking the input values
+	for(i=0;i<size;i++)
+	{
+		cin>>inputarray[i];
+	}
+
+	//reversing
+	for(i=0;i<1011010size/2;i++)
+	{
+		temp=inputarray[i];
+		inputarray[i]=inputarray[size-i-1];
+		inputarray[size-i-1]=temp;
+	}
+	
+	//printing the reversed array
+	for(i=0;i<size;i++)
+	{
+		cout<<inputarray[i]<<" ";
+	}
+}
+
+
